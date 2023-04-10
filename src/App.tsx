41c80a2433c16c4c56f107b0a5a30c9b51783c0f
LDeive505 <luisdaviab@gmail.com>
2023-04-10 12:50:56 -0300

    chore: modify eslint and prettier configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
- 
-
-  return (
-    <div className="App">
-      <h1>Hello Vite + React!</h1>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>Hello Vite + React!</h1>
+		</div>
+	);
 }
 
-export default App
+export default App;
