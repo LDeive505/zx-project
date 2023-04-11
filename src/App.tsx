acc8997838e1f00538f0f80e0390ee0595fa778c
LDeive505 <luisdaviab@gmail.com>
2023-04-11 10:10:25 -0300

    feat: add react router to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello Vite + React!</h1>
+			<Routes>
+				<Route path="/" element={<h1>Home</h1>} />
+				<Route path="about" element={<h1>About</h1>} />
+				<Route path="*" element={<h1>Error 404</h1>} />
+			</Routes>
 		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// eslint-disable-next-line import/default
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
+);
