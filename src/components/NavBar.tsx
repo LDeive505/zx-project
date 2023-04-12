744255a6cb89f547615ba5a87ac25bd961f3e0b4
LDeive505 <luisdaviab@gmail.com>
2023-04-12 12:07:23 -0300

    feat: implement the home page input and add some simple css for vizualization

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import '../styles/componentStyles/NavBar.css';
 
 export default function NavBar() {
 	return (
-		<nav>
-			<ul >
+		<nav className="navbar">
+			<ul>
 				<li>
 					<Link to="/">Home</Link>
 				</li>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,26 @@
 import NavBar from '../components/NavBar';
+import { ChangeEvent, useState } from 'react';
 
 export default function Home() {
+	const [inputSearch, setInputSearch] = useState<string>('');
+
+	const onInputChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+		setInputSearch(target.value);
+	};
+
 	return (
 		<div>
 			<NavBar />
-			<h1>Homer</h1>
+			<h1>Welcome to the Zelda Collection</h1>
+			<label htmlFor="search-input">
+				Search for a zelda game:
+				<input
+					type="text"
+					id="search-input"
+					value={inputSearch}
+					onChange={onInputChange}
+				/>
+			</label>
 		</div>
 	);
 }
